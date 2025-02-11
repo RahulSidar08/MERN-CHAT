88a2e7420d11fe818cdf180d23916d674bad5980
RahulSidar08 <rahulsidar2056@gmail.com>
2025-02-11 22:17:24 +0530

    deployement set up

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path"
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
 import { connectDb } from "./config/dbConfig.js";
 dotenv.config();
-
+const _dirname = path.resolve()
 // middleware
 app.use(express.json());
 app.use(cookieParser());
@@ -29,6 +29,13 @@ connectDb()
 //routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+
+// deployment code 
+app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
