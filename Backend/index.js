342bf3b17ae86eb89609ef3cc52a5d5fe599fa55
RahulSidar08 <rahulsidar2056@gmail.com>
2025-02-11 22:45:32 +0530

    fixing

--- Backend/index.js
+++ Backend/index.js
@@ -32,9 +32,9 @@ app.use("/api/message", messageRoute);
 
 
 // deployment code 
-app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.use(express.static(path.join(_dirname,"/Frontend/dist")))
 app.get('*', (req,res) => {
-    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
+    res.sendFile(path.resolve(_dirname,"Frontend","dist","index.html"))
 })
 
 server.listen(PORT, () => {
